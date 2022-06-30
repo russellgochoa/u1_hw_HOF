8541f694e85bfe1ab5aebcd3be2d307ff5f90fd7
Russell Ochoa <ochoa.russell@gmail.com>
2022-06-29 23:31:28 -0500

    u1_hw_HOF

--- solution.js
+++ solution.js
@@ -5,6 +5,11 @@ function printNums(nums) {
    *  Using .forEach, add each number in the nums array to a new array.
    * @param nums => [1,2,3,4]
    */
+  const arrayToReturn = []
+  nums.forEach((num) => {
+    arrayToReturn.push(num)
+  })
+  return arrayToReturn
 }
 
 function returnSums(nums) {
@@ -13,14 +18,23 @@ function returnSums(nums) {
    * to a new array and return the new array
    * @param nums => [1,2,3,4,5]
    */
+  const newArray = []
+  nums.forEach((num, index) => {
+    newArray.push(index + num)
+  })
+  return newArray
 }
 
+let sum = 0
 function returnTotal(objs) {
   /**
    * Using forEach return a number that is the total sum of all numbers in the array of objects.
    * The key for each object will be n
    * @param objs => [{n:1}]
    */
+  objs.forEach((obj) => {})
+    returnTotal
+  return objs
 }
 
 function printMoney(decimals) {
@@ -33,6 +47,9 @@ function printMoney(decimals) {
    * }
    * Hint: Not all decimals have two places, make sure to return each decimal with the proper decimal places. .toFixed may be useful :)
    */
+  const dollarArray = decimals.map((decimal) => {
+    decimals.toFixed
+  })
 }
 
 function returnAllTrueValues(values) {
@@ -40,6 +57,9 @@ function returnAllTrueValues(values) {
    * @param values => [{name: 'Joe', happy:false}]
    * Using .filter, return an array of objects where happy equals true
    */
+  const arrayOfValues = []
+  values.filter((happy) => {})
+  return happy === true
 }
 
 function addTwo(nums) {
@@ -48,6 +68,9 @@ function addTwo(nums) {
    * Using .reduce, return the total sum of all numbers.
    * Each number should have 2 added to it.
    */
+   const sum = nums.reduce((total) => {
+    return total + 2
+  }, 0)
   //   3
   //   4 => 7
 }
@@ -57,7 +80,10 @@ function joinStrings(strings) {
    * @param strings => ['g', 'f', 'z']
    * Using .reduce, return a new string containing each string from the strings array.
    */
-}
+  strings.reduce((accumulator, string) => {
+    return accumlator + string
+  }, 0
+})
 
 //Bonus
 function sortObjectsByValue(objs) {
